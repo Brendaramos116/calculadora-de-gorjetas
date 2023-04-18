1b4e649902f53cdc60f009feb928200707a779ea
unknown <brendaramos116@hotmail.com>
2023-04-17 21:19:36 -0300

    second commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function calculateTips(event) {
     let bill = document.getElementById('bill').value;
     let serviquali = document.getElementById('serviquali').value;
     let numOfPeople = document.getElementById('people').value;
-
+    console.log(bill, serviquali, numOfPeople)
     if (bill == '' | serviquali == 0) {
         alert('Por favor, preencha os valores');
         return;
@@ -18,11 +18,13 @@ function calculateTips(event) {
 
     let total = (bill * serviquali) / numOfPeople;
     total = total.toFixed(2);
+    each = total / numOfPeople
     document.getElementById('tip').innerHTML = total;
+    document.getElementById('each').innerHTML = each;
     document.getElementById('totaltips').style.display = 'block';
 }
 
 document.getElementById('totaltips').style.display = 'none';
 document.getElementById('each').style.display = 'none';
 
-document.getElementById('tipsform').addEventListener('submit', calculateTips);
+document.getElementById('botao').addEventListener('click', calculateTips);
